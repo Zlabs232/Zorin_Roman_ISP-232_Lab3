9ac9da8f258eea302d9a79015965fdfa87f78666
Роман Зорин <sgena170315@gmail.com>
2025-09-04 10:46:10 +0300

    Задание 4. Таблица умножения

--- Zorin_Roman_ISP-232_Lab3/Program.cs
+++ Zorin_Roman_ISP-232_Lab3/Program.cs
@@ -1,12 +1,14 @@
-﻿namespace Program
+﻿using System.Runtime.ExceptionServices;
+
+namespace Program
 {
     class Program
     {
         static void Main()
         {
-            for (int i = 10; i > 0; i--)
+            for (int i = 1; i <= 10; i++)
             {
-                Console.WriteLine(i);
+                Console.WriteLine($"5 x {i} = {i*5}");
             }
         }
     }
