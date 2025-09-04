39567022a15f931bc5847b056b5e4ff586ba2f44
Роман Зорин <sgena170315@gmail.com>
2025-09-04 10:44:48 +0300

    Задание 3. Обратный отсчёт

--- Zorin_Roman_ISP-232_Lab3/Program.cs
+++ Zorin_Roman_ISP-232_Lab3/Program.cs
@@ -2,28 +2,12 @@
 {
     class Program
     {
-        enum OrderStatus { New, Processing, Shipped, Delivered}
-
         static void Main()
         {
-            OrderStatus[] test =
+            for (int i = 10; i > 0; i--)
             {
-                OrderStatus.New,
-                OrderStatus.Processing,
-                OrderStatus.Shipped,
-                OrderStatus.Delivered,
-            };
-
-            foreach(var status in test)
-            {
-                bool canC = Check(status);
-                Console.WriteLine($"Статус заказа: {status}, Можно ли отменить: {(canC == true ? "Да" : "Нет")}");
+                Console.WriteLine(i);
             }
-        }
-
-        static bool Check(OrderStatus status)
-        {
-            return status == OrderStatus.New || status == OrderStatus.Processing;
         }
     }
 }
