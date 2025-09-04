2bbffe5d87b6f42957b67e7d92166e01f4e07cf5
Роман Зорин <sgena170315@gmail.com>
2025-09-04 11:14:35 +0300

    Задание 9: Ключевое слово «Стоп»

--- Zorin_Roman_ISP-232_Lab3/Program.cs
+++ Zorin_Roman_ISP-232_Lab3/Program.cs
@@ -4,9 +4,24 @@ class Program
 {
     static void Main()
     {
-        bool Even = true;
-        for (int i = 0; i < 10; i++)
-            if (int.Parse(Console.ReadLine()) % 2 != 0) Even = false;
-        Console.WriteLine(Even ? "YES" : "NO");
+
+        Console.WriteLine("Здарова, ты попал на стрельбище!!");
+        Console.WriteLine("Введите 'Stop' если хотите завершить стерльбу");
+        int n = 0;
+        while (true)
+        {
+            Console.WriteLine("Нажмите Enter для выстрела: ");
+            string enter = Console.ReadLine().ToLower();
+            if (enter == "стоп")
+            {
+                Console.WriteLine($"Выстрелов совершено {n}");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Выстрел совершен.");
+                ++n;
+            }
+        }
     }
 }
